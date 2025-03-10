e41d17c2e33a7737e38887de7437d329d1af2cde
Mikhail <81607649+blurmb@users.noreply.github.com>
2025-03-10 20:44:04 +0300

    реализация Button

---
+++ src/shared/ui/Button/index.tsx
@@ -0,0 +1,36 @@
+import classNames from "classnames";
+import classes from "./Button.module.scss";
+
+export type ButtonProps = {
+  children: React.ReactNode;
+  variant?: "primary";
+  onClick?: () => void;
+  size?: "sm" | "md" | "lg";
+  disabled?: boolean;
+  className?: string;
+};
+
+export const Button = ({
+  children,
+  variant = "primary",
+  onClick,
+  size = "md",
+  disabled,
+  className,
+}: ButtonProps) => {
+  console.log(classes[variant]);
+  return (
+    <button
+      className={classNames(
+        classes.button,
+        classes[variant],
+        classes[size],
+        className,
+      )}
+      onClick={onClick}
+      disabled={disabled}
+    >
+      {children}
+    </button>
+  );
+};
