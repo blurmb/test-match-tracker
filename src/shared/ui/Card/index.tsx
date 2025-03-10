f88ea8187ecc3bb836ea00b57ca6948de6035b63
Mikhail <81607649+blurmb@users.noreply.github.com>
2025-03-10 22:49:21 +0300

    extracted CardVariant type

--- src/shared/ui/Card/index.tsx
+++ src/shared/ui/Card/index.tsx
@@ -2,10 +2,16 @@ import classNames from "classnames";
 
 import classes from "./Card.module.scss";
 
+export type CardVariant =
+  | "dark"
+  | "darker"
+  | "accent"
+  | "accentSecondary"
+  | "primary";
 export type CardProps = {
   children: React.ReactNode;
   className?: string;
-  variant?: "dark" | "darker" | "accent" | "accentSecondary" | "primary";
+  variant?: CardVariant;
 };
 export const Card = ({ children, className, variant = "dark" }: CardProps) => (
   <div className={classNames(classes.card, classes[variant], className)}>
