cfcba94539f69e4c6edca8632b3c63bddc2f16e1
Mikhail <81607649+blurmb@users.noreply.github.com>
2025-03-11 01:21:49 +0300

    скелетон при загрузке и ошибке

--- src/widgets/MatchList/ui/index.tsx
+++ src/widgets/MatchList/ui/index.tsx
@@ -1,12 +1,25 @@
 import classes from "./MatchList.module.scss";
-import { getMatches, getMatchesError } from "@src/entities/Match";
-import { ListItem } from "./ListItem";
+import {
+  getMatches,
+  getMatchesError,
+  getMatchesLoading,
+} from "@src/entities/Match";
+import { ListItem, ListItemSkeleton } from "./ListItem";
 import { useAppSelector } from "@src/store/hooks";
 
 export const MatchList = () => {
   const data = useAppSelector(getMatches);
+  const isLoading = useAppSelector(getMatchesLoading);
+  const error = useAppSelector(getMatchesError);
+  const isError = (error && error !== "aborted") || false;
   return (
     <div className={classes.wrapper}>
+      {data.length === 0 &&
+        Array(5)
+          .fill(0)
+          .map((_, i) => (
+            <ListItemSkeleton key={i} isLoading={isLoading} isError={isError} />
+          ))}
       {data.map((match) => (
         <ListItem key={match.title} match={match} />
       ))}
