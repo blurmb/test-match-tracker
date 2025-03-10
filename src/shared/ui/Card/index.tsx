0f3cfaa5f489dbcba0a7dd2d7e9ddb69d13d936a
Mikhail <81607649+blurmb@users.noreply.github.com>
2025-03-10 22:32:18 +0300

    более темный фон карточки

--- src/shared/ui/Card/index.tsx
+++ src/shared/ui/Card/index.tsx
@@ -5,7 +5,10 @@ import classes from "./Card.module.scss";
 export type CardProps = {
   children: React.ReactNode;
   className?: string;
+  variant?: "dark" | "darker";
 };
-export const Card = ({ children, className }: CardProps) => (
-  <div className={classNames(classes.card, className)}>{children}</div>
+export const Card = ({ children, className, variant = "dark" }: CardProps) => (
+  <div className={classNames(classes.card, classes[variant], className)}>
+    {children}
+  </div>
 );
