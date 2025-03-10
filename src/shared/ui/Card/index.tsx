cad6c56fb9cf5ab854ca2f08cdbc34f1a9422b7a
Mikhail <81607649+blurmb@users.noreply.github.com>
2025-03-10 22:40:42 +0300

    Card more colors support

--- src/shared/ui/Card/index.tsx
+++ src/shared/ui/Card/index.tsx
@@ -5,7 +5,7 @@ import classes from "./Card.module.scss";
 export type CardProps = {
   children: React.ReactNode;
   className?: string;
-  variant?: "dark" | "darker";
+  variant?: "dark" | "darker" | "accent" | "accentSecondary" | "primary";
 };
 export const Card = ({ children, className, variant = "dark" }: CardProps) => (
   <div className={classNames(classes.card, classes[variant], className)}>
