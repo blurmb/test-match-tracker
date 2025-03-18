4c55f5a4aa922eec88fda65ac14c6578ebbbdcf8
Mikhail <81607649+blurmb@users.noreply.github.com>
2025-03-18 22:57:02 +0300

    small team logo

--- src/shared/assets/icons/index.ts
+++ src/shared/assets/icons/index.ts
@@ -1,5 +1,6 @@
 import RefreshIcon from "./refresh.svg";
 import AlertIcon from "./alert.svg";
 import DefaultTeamLogo from "./default-team-logo.svg";
+import DefaultTeamLogoSmall from "./default-team-logo-28.svg";
 
-export { RefreshIcon, AlertIcon, DefaultTeamLogo };
+export { RefreshIcon, AlertIcon, DefaultTeamLogo, DefaultTeamLogoSmall };
