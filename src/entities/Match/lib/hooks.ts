9ded42ee17e1854c701b1e564bcb7710db4f7ae9
Mikhail <81607649+blurmb@users.noreply.github.com>
2025-03-18 03:08:31 +0300

    обработка ошибок и таймауты

--- src/entities/Match/lib/hooks.ts
+++ src/entities/Match/lib/hooks.ts
@@ -1,28 +1,64 @@
 import { MatchesApi, matchesApi } from "@src/shared/api";
 import { useAppDispatch } from "@src/store/hooks";
 import { useEffect } from "react";
-import { setError, setMatches } from "../model/slice";
+import { setError, setIsLoading, setMatches } from "../model/slice";
 
 type DispatchCB = (matches: MatchesApi.MatchSocketMessage) => void;
+type WebSocketErrorCB = (error: CloseEvent) => void;
+const CONNECT_TIMEOUT_MS = 5000;
+const INACTIVE_TIMEOUT_MS = 5000;
 class SocketService {
   socket?: WebSocket;
   private dispatch?: DispatchCB;
+  private onError?: WebSocketErrorCB;
+  private connectTimeout?: ReturnType<typeof setTimeout>;
+  private inactiveTimeout?: ReturnType<typeof setTimeout>;
+  private inactiveTimeoutCB = () => {
+    this.socket?.close(4002, "Inactivity timeout");
+  };
+  private updateInactiveTimeout = () => {
+    clearTimeout(this.inactiveTimeout);
+    this.inactiveTimeout = setTimeout(
+      this.inactiveTimeoutCB,
+      INACTIVE_TIMEOUT_MS,
+    );
+  };
+  private onClose = (event: CloseEvent) => {
+    if (event.code !== 1000) {
+      this.onError?.(event);
+    }
+  };
   private onMessage = (event: MessageEvent) => {
     const data = JSON.parse(event.data) as MatchesApi.MatchSocketMessage;
+    this.updateInactiveTimeout();
     this.dispatch?.(data);
   };
-  connect(dispatch: DispatchCB) {
+  connect = (dispatch: DispatchCB, onError: WebSocketErrorCB) => {
     if (this.socket) {
       throw new Error("Socket is already connected");
     }
     this.socket = matchesApi.createMatchesSocket();
     this.dispatch = dispatch;
+    this.onError = onError;
     this.socket.addEventListener("message", this.onMessage);
-  }
-  disconnect() {
+    this.socket.addEventListener("close", this.onClose);
+    this.socket.addEventListener("open", () => {
+      clearTimeout(this.connectTimeout);
+      this.updateInactiveTimeout();
+    });
+    this.connectTimeout = setTimeout(() => {
+      this.socket?.close(4001, "Connection timeout");
+    }, CONNECT_TIMEOUT_MS);
+  };
+  disconnect = () => {
+    clearTimeout(this.connectTimeout);
+    clearTimeout(this.inactiveTimeout);
+
+    this.socket?.removeEventListener("message", this.onMessage);
+    this.socket?.removeEventListener("close", this.onClose);
     this.socket?.close();
     this.socket = undefined;
-  }
+  };
 }
 const socketService: SocketService = new SocketService();
 
@@ -37,8 +73,12 @@ export const useMatchesAutoUpdate = __FEATURES__.AUTO_UPDATE
         }
         dispatch(setMatches(matches.data));
       };
+      const onError = (error: CloseEvent) => {
+        console.error("WebSocket error", error);
+        dispatch(setError("WebSocket error"));
+      };
       useEffect(() => {
-        socketService.connect(dispatchCB);
+        socketService.connect(dispatchCB, onError);
         return () => {
           socketService.disconnect();
         };
